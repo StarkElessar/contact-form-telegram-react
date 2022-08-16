d7f12272384890116e5306f961b98bee9a1c15af
Stark <serj.elessar@gmail.com>
2022-08-16 19:28:38 +0300

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          DataToTelegram
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
