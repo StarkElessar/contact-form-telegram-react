d37d04348742996126abb1ea7c7418f9856423b2
Stark <serj.elessar@gmail.com>
2022-08-16 19:28:32 +0300

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__copyright">
+          <span>StarkElessar 2022</span>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
